e777d3b4903f2dd4bcc543cc176e87161aa6cd6b
KirylMisiuk <kirill.misiuk@gmail.com>
2020-02-24 17:11:43 +0300

    fix

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { databaseProviders } from './databse.providers';
 
 @Module({
-  providers: [...databaseProviders],
-  exports: [...databaseProviders],
+    providers: [...databaseProviders],
+    exports: [...databaseProviders],
 })
 export class DatabaseModule {}
